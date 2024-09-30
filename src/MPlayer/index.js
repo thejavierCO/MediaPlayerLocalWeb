e65e07d6fce1b94bc57767e960b8496861640142
javier cruz obispo <javier.cruz.odispo@hotmail.com>
2024-09-30 11:50:51 -0600

    create class

--- src/MPlayer/index.js
+++ src/MPlayer/index.js
@@ -1,6 +1,20 @@
 class MPlayer {
   constructor(query) {
-    let tag = document.querySelector(query)
-    console.log(tag)
+    this.tag = document.querySelector(query)
+  }
+  get src() {
+    return this.tag.src;
+  }
+  set src(data) {
+    this.tag.src = data;
+  }
+  play() {
+    return this.tag.play();
+  }
+  pause() {
+    return this.tag.pause();
+  }
+  autoplay() {
+    this.tag.autoplay = true;
   }
 }
